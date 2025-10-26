ad983f2f240aaefd36ebeffe15e9c8195eff3817
Minimorum75 <huddle-room.0g@icloud.com>
2025-10-26 20:32:46 +0100

    Create regional_cards_data.js

---
+++ regional_cards_data.js
@@ -0,0 +1,345 @@
+// Auto-generated regional train cards data
+// Last updated: October 2025
+// DO NOT EDIT MANUALLY
+
+const regionalCards = {
+  "auvergne-rhone-alpes": {
+    "name": "Auvergne-Rhône-Alpes",
+    "system": "TER Auvergne-Rhône-Alpes",
+    "website": "https://ter.sncf.com/auvergne-rhone-alpes",
+    "cards": [
+      {
+        "name": "Illico Liberté Jeunes",
+        "age": "12-25 ans",
+        "price": 30.0,
+        "validity": "1 an",
+        "discount": "Jusqu'à -50%",
+        "description": "Carte de réduction pour les trajets TER en région",
+        "url": "https://ter.sncf.com/auvergne-rhone-alpes/offres/abonnements-cartes"
+      },
+      {
+        "name": "Illico Liberté",
+        "age": "26 ans et +",
+        "price": 75.0,
+        "validity": "1 an",
+        "discount": "-25% à -50%",
+        "description": "Carte de réduction pour les trajets TER en région",
+        "url": "https://ter.sncf.com/auvergne-rhone-alpes/offres/abonnements-cartes"
+      }
+    ]
+  },
+  "bretagne": {
+    "name": "Bretagne",
+    "system": "BreizhGo",
+    "website": "https://www.breizhgo.bzh",
+    "cards": [
+      {
+        "name": "KorriGo Jeunes",
+        "age": "Moins de 26 ans",
+        "price": 15.0,
+        "validity": "1 an",
+        "discount": "-50%",
+        "description": "Carte de réduction BreizhGo pour les jeunes",
+        "url": "https://www.breizhgo.bzh/se-deplacer-en-bretagne/titres-de-transport"
+      },
+      {
+        "name": "KorriGo",
+        "age": "Tous âges",
+        "price": 35.0,
+        "validity": "1 an",
+        "discount": "-25%",
+        "description": "Carte de réduction BreizhGo",
+        "url": "https://www.breizhgo.bzh/se-deplacer-en-bretagne/titres-de-transport"
+      }
+    ]
+  },
+  "bourgogne-franche-comte": {
+    "name": "Bourgogne-Franche-Comté",
+    "system": "Mobigo",
+    "website": "https://www.viamobigo.fr",
+    "cards": [
+      {
+        "name": "Mobigo Jeunes",
+        "age": "Moins de 26 ans",
+        "price": 30.0,
+        "validity": "1 an",
+        "discount": "-50%",
+        "description": "Carte de réduction pour les trajets TER en région",
+        "url": "https://www.viamobigo.fr/titres-et-tarifs/cartes-de-reduction/"
+      },
+      {
+        "name": "Mobigo",
+        "age": "26 ans et +",
+        "price": 75.0,
+        "validity": "1 an",
+        "discount": "-25%",
+        "description": "Carte de réduction pour les trajets TER en région",
+        "url": "https://www.viamobigo.fr/titres-et-tarifs/cartes-de-reduction/"
+      }
+    ]
+  },
+  "centre-val-de-loire": {
+    "name": "Centre-Val de Loire",
+    "system": "Rémi",
+    "website": "https://www.remi-centrevaldeloire.fr",
+    "cards": [
+      {
+        "name": "Rémi Jeunes",
+        "age": "Moins de 26 ans",
+        "price": 30.0,
+        "validity": "1 an",
+        "discount": "-50%",
+        "description": "Carte de réduction Rémi pour les jeunes",
+        "url": "https://www.remi-centrevaldeloire.fr/cartes-abonnements/"
+      },
+      {
+        "name": "Rémi",
+        "age": "26 ans et +",
+        "price": 50.0,
+        "validity": "1 an",
+        "discount": "-25%",
+        "description": "Carte de réduction Rémi",
+        "url": "https://www.remi-centrevaldeloire.fr/cartes-abonnements/"
+      }
+    ]
+  },
+  "corse": {
+    "name": "Corse",
+    "system": "Chemins de fer de la Corse",
+    "website": "https://cf-corse.corsica",
+    "cards": [
+      {
+        "name": "Carte Annuelle Jeune",
+        "age": "Moins de 26 ans",
+        "price": 50.0,
+        "validity": "1 an",
+        "discount": "Tarif préférentiel",
+        "description": "Carte annuelle pour les trajets en Corse",
+        "url": "https://cf-corse.corsica/"
+      },
+      {
+        "name": "Carte Annuelle",
+        "age": "Tous âges",
+        "price": 80.0,
+        "validity": "1 an",
+        "discount": "Tarif préférentiel",
+        "description": "Carte annuelle pour les trajets en Corse",
+        "url": "https://cf-corse.corsica/"
+      }
+    ]
+  },
+  "grand-est": {
+    "name": "Grand Est",
+    "system": "TER Fluo Grand Est",
+    "website": "https://www.fluo.eu",
+    "cards": [
+      {
+        "name": "Fluo Jeunes",
+        "age": "Moins de 26 ans",
+        "price": 0.0,
+        "validity": "1 an",
+        "discount": "-50%",
+        "description": "Carte gratuite pour les moins de 26 ans",
+        "url": "https://www.fluo.eu/titres-tarifs/cartes-abonnements"
+      },
+      {
+        "name": "Fluo",
+        "age": "26 ans et +",
+        "price": 25.0,
+        "validity": "1 an",
+        "discount": "-25% à -50%",
+        "description": "Carte de réduction TER Grand Est",
+        "url": "https://www.fluo.eu/titres-tarifs/cartes-abonnements"
+      }
+    ]
+  },
+  "hauts-de-france": {
+    "name": "Hauts-de-France",
+    "system": "TER Hauts-de-France",
+    "website": "https://ter.sncf.com/hauts-de-france",
+    "cards": [
+      {
+        "name": "Abonnement TER Jeunes",
+        "age": "Moins de 26 ans",
+        "price": null,
+        "validity": "Variable",
+        "discount": "Selon trajet",
+        "description": "Abonnements mensuels à tarif réduit",
+        "url": "https://ter.sncf.com/hauts-de-france/offres/abonnements"
+      },
+      {
+        "name": "Abonnement TER",
+        "age": "Tous âges",
+        "price": null,
+        "validity": "Variable",
+        "discount": "Selon trajet",
+        "description": "Abonnements mensuels ou annuels",
+        "url": "https://ter.sncf.com/hauts-de-france/offres/abonnements"
+      }
+    ]
+  },
+  "ile-de-france": {
+    "name": "Île-de-France",
+    "system": "Navigo",
+    "website": "https://www.iledefrance-mobilites.fr",
+    "cards": [
+      {
+        "name": "Navigo Jeunes (week-end)",
+        "age": "Moins de 26 ans",
+        "price": 4.0,
+        "validity": "1 jour (sam/dim)",
+        "discount": "Forfait illimité",
+        "description": "Forfait week-end pour les jeunes",
+        "url": "https://www.iledefrance-mobilites.fr/titres-et-tarifs"
+      },
+      {
+        "name": "Navigo Annuel Jeunes",
+        "age": "Étudiants -26 ans",
+        "price": 350.0,
+        "validity": "1 an",
+        "discount": "Toutes zones illimité",
+        "description": "Forfait annuel étudiant toutes zones",
+        "url": "https://www.iledefrance-mobilites.fr/titres-et-tarifs"
+      },
+      {
+        "name": "Navigo Annuel",
+        "age": "Tous âges",
+        "price": 827.0,
+        "validity": "1 an",
+        "discount": "Toutes zones illimité",
+        "description": "Forfait annuel toutes zones",
+        "url": "https://www.iledefrance-mobilites.fr/titres-et-tarifs"
+      }
+    ]
+  },
+  "normandie": {
+    "name": "Normandie",
+    "system": "Atoumod",
+    "website": "https://www.atoumod.fr",
+    "cards": [
+      {
+        "name": "Atoumod Étudiant",
+        "age": "Étudiants -28 ans",
+        "price": 30.0,
+        "validity": "1 mois",
+        "discount": "Forfait illimité",
+        "description": "Abonnement mensuel étudiant",
+        "url": "https://www.atoumod.fr/titres-et-tarifs/"
+      },
+      {
+        "name": "Atoumod Mensuel",
+        "age": "Tous âges",
+        "price": null,
+        "validity": "1 mois",
+        "discount": "Variable selon trajet",
+        "description": "Abonnement mensuel selon zone",
+        "url": "https://www.atoumod.fr/titres-et-tarifs/"
+      }
+    ]
+  },
+  "nouvelle-aquitaine": {
+    "name": "Nouvelle-Aquitaine",
+    "system": "Modalis",
+    "website": "https://ter.sncf.com/nouvelle-aquitaine",
+    "cards": [
+      {
+        "name": "Modalis Jeunes",
+        "age": "Moins de 26 ans",
+        "price": 0.0,
+        "validity": "1 an",
+        "discount": "-50%",
+        "description": "Carte gratuite pour les jeunes",
+        "url": "https://ter.sncf.com/nouvelle-aquitaine/offres/cartes-abonnements"
+      },
+      {
+        "name": "Modalis",
+        "age": "26 ans et +",
+        "price": 30.0,
+        "validity": "1 an",
+        "discount": "-25% à -50%",
+        "description": "Carte de réduction TER Nouvelle-Aquitaine",
+        "url": "https://ter.sncf.com/nouvelle-aquitaine/offres/cartes-abonnements"
+      }
+    ]
+  },
+  "occitanie": {
+    "name": "Occitanie",
+    "system": "liO",
+    "website": "https://lio.laregion.fr",
+    "cards": [
+      {
+        "name": "liO Jeunes",
+        "age": "Moins de 26 ans",
+        "price": 1.0,
+        "validity": "1 an",
+        "discount": "-50%",
+        "description": "Carte à 1€ pour les jeunes",
+        "url": "https://lio.laregion.fr/carte-lio-train"
+      },
+      {
+        "name": "liO Train",
+        "age": "26 ans et +",
+        "price": 30.0,
+        "validity": "1 an",
+        "discount": "-25% à -50%",
+        "description": "Carte de réduction liO Train",
+        "url": "https://lio.laregion.fr/carte-lio-train"
+      }
+    ]
+  },
+  "pays-de-la-loire": {
+    "name": "Pays de la Loire",
+    "system": "Aléop",
+    "website": "https://aleop.paysdelaloire.fr",
+    "cards": [
+      {
+        "name": "Aléop Jeunes",
+        "age": "Moins de 26 ans",
+        "price": 30.0,
+        "validity": "1 an",
+        "discount": "-50%",
+        "description": "Carte de réduction Aléop pour les jeunes",
+        "url": "https://aleop.paysdelaloire.fr/cartes-tarifs/"
+      },
+      {
+        "name": "Aléop",
+        "age": "26 ans et +",
+        "price": 50.0,
+        "validity": "1 an",
+        "discount": "-25%",
+        "description": "Carte de réduction Aléop",
+        "url": "https://aleop.paysdelaloire.fr/cartes-tarifs/"
+      }
+    ]
+  },
+  "provence-alpes-cote-dazur": {
+    "name": "Provence-Alpes-Côte d'Azur",
+    "system": "Zou!",
+    "website": "https://zou.maregionsud.fr",
+    "cards": [
+      {
+        "name": "Zou! Études",
+        "age": "Étudiants -26 ans",
+        "price": 0.0,
+        "validity": "1 an",
+        "discount": "Illimité",
+        "description": "Carte gratuite pour les étudiants",
+        "url": "zou.maregionsud.fr/se-deplacer/titres-tarifs"
+      },
+      {
+        "name": "Zou! Illimité",
+        "age": "Tous âges",
+        "price": null,
+        "validity": "Variable",
+        "discount": "Forfait selon ligne",
+        "description": "Forfait mensuel ou annuel selon ligne",
+        "url": "https://zou.maregionsud.fr/se-deplacer/titres-tarifs"
+      }
+    ]
+  }
+};
+
+// Export for use in HTML
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = { regionalCards };
+}
